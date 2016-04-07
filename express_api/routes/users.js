9485c6f5e63fcf7fa5bc1fe051e01dde2db36e81
JessicaHerford <jrherford12@gmail.com>
2016-04-07 08:24:48 -0600

    Fresh Start

--- express_api/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hit the home page!');
-});
-
-module.exports = router;
