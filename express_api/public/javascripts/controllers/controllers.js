da63ccf2d8795c66bdb1f59a3d498ea375900bca
JessicaHerford <jrherford12@gmail.com>
2016-04-08 15:12:34 -0600

    Added some styling

--- express_api/public/javascripts/controllers/controllers.js
+++ express_api/public/javascripts/controllers/controllers.js
@@ -15,14 +15,7 @@ app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
 
 app.controller('IngredientController', ['$scope', '$http', 'getIngredients',  function($scope, $http, getIngredients){
   console.log(getIngredients);
-  var ingredientsMatched = [];
 
-  // getIngredients.getSome().then(function(payload){
-  //   console.log(payload.data);
-  //   return $scope.ingredients = payload.data;
-  // })
-
-  // console.log(getIngredients.data);
 
   $scope.findIngredients = function(ingredientSearched){
 
@@ -39,42 +32,7 @@ app.controller('IngredientController', ['$scope', '$http', 'getIngredients',  fu
 
 };
 
-    // var pairedIngredients = [];
-    // $http.get('../../json/ingredients.json').success(function(data){
-    // $scope.data = data;
-    // console.log(data[0]);
-    // // console.log(data[0].Apples);
-    // return data[0];
-    // });
-    //   $http.get('../../json/ingredients.json').success(function(data){
-    //   $scope.data = data;
-    //   console.log(data[0]);
-    //   // console.log(data[0].Apples);
-    //   return data[0];
-    // })
 
 
-
-
-// }
 
 }])
-
-
-// var lookup = require('../../json/ingredients.json');
-//
-//
-// listIngredientPairings: function(data, ingredient){
-//       var pairedIngredients = [];
-//
-//       if(data[this.ingredient]){
-//               this.ingredient = data[this.ingredient];
-//               pairedIngredients.push([this.ingredient]);
-//               console.log(pairedIngredients[0][0]);
-//               pairedIngredients = pairedIngredients[0][0];
-//               return pairedIngredients;
-//              }
-//              else{
-//                alert("Please try again, ingredient not found!");
-//              }
-//            }
