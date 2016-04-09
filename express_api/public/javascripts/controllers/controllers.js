ed39ad257b7fe4ad40152f2c0195c54c4b325750
JessicaHerford <jrherford12@gmail.com>
2016-04-09 15:21:06 -0600

    Got ingredients to append

--- express_api/public/javascripts/controllers/controllers.js
+++ express_api/public/javascripts/controllers/controllers.js
@@ -13,8 +13,8 @@ app.controller('MainController', ['$scope', '$http', function ($scope, $http) {
 }])
 
 
-app.controller('IngredientController', ['$scope', '$http', 'getIngredients', function($scope, $http, getIngredients){
-  console.log(getIngredients);
+app.controller('IngredientController', ['$scope', '$http', 'getIngredients',  function($scope, $http, getIngredients){
+  // console.log(getIngredients);
 
 
   $scope.findIngredients = function(ingredientSearched){
@@ -30,12 +30,71 @@ app.controller('IngredientController', ['$scope', '$http', 'getIngredients', fun
       return $scope.ingredients = payload.data;
     })
 
+}
 
+$scope.selectedIngredients = {
+  ingredients: []
+}
 
+$scope.check = function(value, checked) {
+  var idx = $scope.selectedIngredients.ingredients.indexOf(value);
+  if (idx >= 0 && !checked) {
+    $scope.selectedIngredients.ingredients.splice(idx, 1);
+  }
+  if (idx < 0 && checked) {
+    $scope.selectedIngredients.ingredients.push(value);
+  }
+  console.log($scope.selectedIngredients.ingredients);
 };
 
 
-  
+$scope.user = {
+  roles: ['guest']
+};
+
+// $scope.check = function(value, checked) {
+//   var idx = $scope.user.roles.indexOf(value);
+//   if (idx >= 0 && !checked) {
+//     $scope.user.roles.splice(idx, 1);
+//   }
+//   if (idx < 0 && checked) {
+//     $scope.user.roles.push(value);
+//   }
+//   console.log($scope.user.roles);
+// };
+
+
+
+
+// $scope.$watch('items', function(newValue, oldValue) {
+//             console.log(newValue)
+//         }, true);
+
+
+// $scope.checkAll = function() {
+//   $scope.user.roles = $scope.roles.map(function(item) { return item.text; });
+// };
+// $scope.uncheckAll = function() {
+//   $scope.user.roles = [];
+// };
+// $scope.checkFirst = function() {
+//   $scope.user.roles.splice(0, $scope.user.roles.length);
+//   $scope.user.roles.push(1);
+// };
+
+
+
+//    $scope.stateChanged = function (questId) {
+//      var array = [];
+//    if($scope.answers[questId]){ //If it is checked
+//        console.log("Checked");
+//        array.push($scope.answers[questId]);
+//    }
+//    else{
+//      console.log("Unchecked!");
+//    }
+//    console.log(array);
+// }
 
 
 //    $scope.stateChanged = function (questId) {
