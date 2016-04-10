01244a084078b94b22a9a4026167563584b6b660
JessicaHerford <jrherford12@gmail.com>
2016-04-10 08:45:35 -0600

    Added cross origin access

--- express_api/public/javascripts/services/services.js
+++ express_api/public/javascripts/services/services.js
@@ -17,9 +17,9 @@ app.service('getIngredients', ['$http', function($http){
 //     var theData = {};
 //
 //     this.theData = function(){
-//     return $http.get('http://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0');
+//     return $http.get('http://api.bigoven.com/recipes');
 //     }
 //
-//     console.log(theData);
+//     console.log(theData.data);
 //
 // }])
