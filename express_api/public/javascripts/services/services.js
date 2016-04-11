2bb199a29ba02128d09d67ac4c81122802d57b23
JessicaHerford <jrherford12@gmail.com>
2016-04-11 11:40:12 -0600

    changed json

--- express_api/public/javascripts/services/services.js
+++ express_api/public/javascripts/services/services.js
@@ -19,7 +19,7 @@ app.service('food2forkAjaxCall', ['$http', function($http){
 
     this.getData = function(data){
       return $http.get('../../json/food2forkdatasample.json')
-      // return $http.get('http://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0');
+      // return $http.get('http://food2fork.com/api/search?key=');
     }
 
 }])
