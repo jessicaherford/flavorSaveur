d17f415283090cec2d3968589eb18834bde229e0
JessicaHerford <jrherford12@gmail.com>
2016-04-11 21:21:23 -0600

    Working on CORS

--- express_api/public/javascripts/app.js
+++ express_api/public/javascripts/app.js
@@ -1,10 +1,5 @@
 var app = angular.module('flavorsaveur', ['ngRoute', 'ngResource', 'checklist-model']);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.config(function ($routeProvider) {
   $routeProvider
