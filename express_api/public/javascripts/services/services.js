9389792c8709dfad0501df90f55d7adef6bee840
JessicaHerford <jrherford12@gmail.com>
2016-04-11 23:02:00 -0600

    HTTPS issues

--- express_api/public/javascripts/services/services.js
+++ express_api/public/javascripts/services/services.js
@@ -16,10 +16,11 @@ app.service('food2forkAjaxCall', ['$http', function($http){
 
     var food2forkAjaxCall = {};
 
+    //NEED TO FIX THIS!!! DOES NOT WORK ON FIREBASE
 
     this.getData = function(userIngredients){
       userIngredients= userIngredients.join(', ');
-      return $http.get('http://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0&q=' + userIngredients);
+      return $http.get('https://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0&q=' + userIngredients);
     }
 
 }])
