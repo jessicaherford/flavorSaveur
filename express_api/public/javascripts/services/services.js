a02579aa1e03bd408a263e6ee53699cc70361cf0
JessicaHerford <jrherford12@gmail.com>
2016-04-12 09:22:31 -0600

    Moved .evn file

--- express_api/public/javascripts/services/services.js
+++ express_api/public/javascripts/services/services.js
@@ -23,6 +23,7 @@ app.service('food2forkAjaxCall', ['$http', function($http){
       return $http.get('https://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0&q=' + userIngredients);
     }
 
+
 }])
 
 //Trying to pass all contents of $scope.selectedIngredients into search parameters
