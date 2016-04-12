010b8eefc351c42190cafcd0a22480096a0998a7
JessicaHerford <jrherford12@gmail.com>
2016-04-11 21:19:58 -0600

    Added CORS

--- express_api/public/javascripts/app.js
+++ express_api/public/javascripts/app.js
@@ -1,5 +1,11 @@
 var app = angular.module('flavorsaveur', ['ngRoute', 'ngResource', 'checklist-model']);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.config(function ($routeProvider) {
   $routeProvider
   .when('/ingredientsearch', {
