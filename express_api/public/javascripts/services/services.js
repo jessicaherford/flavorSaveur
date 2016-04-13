aecf53fb795dc522d106f9c689669b450d0b0b7d
JessicaHerford <jrherford12@gmail.com>
2016-04-12 22:44:59 -0600

    Changed http request

--- express_api/public/javascripts/services/services.js
+++ express_api/public/javascripts/services/services.js
@@ -20,8 +20,8 @@ app.service('food2forkAjaxCall', ['$http', function($http){
 
     this.getData = function(userIngredients){
       userIngredients= userIngredients.join(', ')
-      // return $http.get('http://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0&q=' + userIngredients);
-      return $http.get('../../json/food2forkdatasample.json');
+      return $http.get('http://food2fork.com/api/search?key=c24b2377e69e34d6b450d0b43e35c9e0&q=' + userIngredients);
+      // return $http.get('../../json/food2forkdatasample.json');
     }
 
 
