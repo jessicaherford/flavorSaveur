25a4b35cdebe63874d0be712e283fa7168041695
JessicaHerford <jrherford12@gmail.com>
2016-04-18 20:25:52 -0600

    removed old CORS code

--- express_api/public/javascripts/app.js
+++ express_api/public/javascripts/app.js
@@ -1,17 +1,17 @@
 var app = angular.module('flavorsaveur', ['ngRoute', 'ngResource', 'checklist-model']);
 
-// app.config(['$httpProvider', function($httpProvider) {
-//         $httpProvider.defaults.useXDomain = true;
-//         delete $httpProvider.defaults.headers.common['X-Requested-With'];
-//     }
-// ]);
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
 
-app.config('/*', function (request, response, next) {
-     response.header("Access-Control-Allow-Origin", "*");
-     response.header("Access-Control-Allow-Headers", "X-Requested-With");
-     response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
-     next();
- });
+// app.config('/*', function (request, response, next) {
+//      response.header("Access-Control-Allow-Origin", "*");
+//      response.header("Access-Control-Allow-Headers", "X-Requested-With");
+//      response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
+//      next();
+//  });
 
 app.config(function ($routeProvider) {
   $routeProvider
